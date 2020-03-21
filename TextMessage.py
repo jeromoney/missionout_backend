252cd6ab5ce78b663ac2e19f5041b31b5686f742
Howard Matis <hsmatis@gmail.com>
2020-03-20 17:38:03 -0700

    Now doing doing text messages for Version 2.

--- TextMessage.py
+++ TextMessage.py
@@ -1,45 +1,45 @@
 import FirebaseSetup
-from Data import Message
+from Secrets import twilio_secrets
+from Data import Message, Team
 import json
-from urllib import parse
 from twilio.rest import Client
-from config import ACCOUNT_SID, AUTH_TOKEN
+from datetime import datetime
 
 #
 # Send Text Messages
 #
-Version = "1.0"
-print("Running Version", Version)
-# brackets are holders for description and needForAction respectively
-CALL_SCRIPT_URL = "https://handler.twilio.com/twiml/EH1dd19d1980e983d0ffbad12486659c20?description={}&needForAction={}"
 
 
-def text_message(event):
-
-    account_sid = ACCOUNT_SID
-    auth_token = AUTH_TOKEN
+def text_message(event, account_sid, auth_token):
     client = Client(account_sid, auth_token)
+    team = Team('chaffeecountysarnorth.org') # TODO- this string is hardcoded and I still need to read the json file
+    voicePhoneNumbers = team.get_voice_phone_numbers()
+    cellPhoneNumbers = team.get_mobile_phone_numbers()
+    message = Message(event)
 
     # These need to be generated automatically
-    cell_number = '+15107170201'
+    cell_number = '+15106290201'
     from_number = '+14069241940'
-    body_message = "Another missing hiker found at end of the rainbow again and again"
     text_image = "https://www.colorpsychology.org/wp-content/uploads/2017/03/rainbow-symbolism.jpg"
-
+    body = F"Chaffee County SAR Mission. {message.description} {message.needForAction} {message.creator}"
     text_call = client.messages.create(
+        body=body,
         to=cell_number,
         from_=from_number,
-        body=body_message,
         media_url=text_image,
     )
     print("Status of Text Call", text_call)
 
+
 def gcf_entry(event):
     FirebaseSetup.setup_firebase_gcf_environment()
-    text_message(event)
+    account_sid, auth_token = twilio_secrets()
+    text_message(event, account_sid, auth_token)
 
 
 if __name__ == '__main__':
+    Version = "2.0"
+    print("Running TextMessage Version", Version, " - ", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     FirebaseSetup.setup_firebase_local_environment()
     TEST_RESOURCE_STR = '{\"oldValue\": {}, \"updateMask\": {}, \"value\": {\"createTime\": \"2020-02-20T22:08:29.494223Z\", ' \
                         '\"fields\": {\"needForAction\": {\"stringValue\": \"we need kisses for the puppy\"}, \"address\": {' \
@@ -48,5 +48,10 @@ if __name__ == '__main__':
                         '\"2020-02-20T22:08:29.415Z\"}}, \"name\": \"projects/missionout/databases/(' \
                         'default)/documents/teams/chaffeecountysarnorth.org/missions/wLZ6aZy6pVRBJBCkZXeu/pages' \
                         '/icGulF5jyDuqBMnyuD2I\", \"updateTime\": \"2020-02-20T22:08:29.494223Z\"}}'
+
     test_event = json.loads(TEST_RESOURCE_STR)
-    text_message(test_event)
+
+    from config import ACCOUNT_SID, AUTH_TOKEN  # This will break if run from cloud
+
+    text_message(test_event, ACCOUNT_SID, AUTH_TOKEN)
+    print("All done with TextMessage -", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))

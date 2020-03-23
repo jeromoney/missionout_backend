3bbb1bcf3abfa2567af4bd0af492779ef63dae2c
jeromoney <justin.matis.com@gmail.com>
2020-03-23 11:26:42 -0600

    added timestamp feature

--- Email.py
+++ Email.py
@@ -1,6 +1,7 @@
 import json
 from firebase_admin import firestore
 
+
 import FirebaseSetup
 from Data import Team, Message
 from Utils import TEST_RESOURCE_STR, get_teamID_from_event
@@ -10,6 +11,8 @@ def send_email(event: dict, team: Team):
     db = firestore.client()
     message = Message(event)
     email = message.get_email()
+    #timestamp email
+    email['timestamp']= firestore.SERVER_TIMESTAMP #SERVER_TIMESTAMP exists, not sure why IDE can't find it
     db.collection('mail').document().set(email)
 
 

b7698dfa70fe27e51fb75a485583867260782eb7
Justin <justin.matis.com@gmail.com>
2020-04-28 15:03:49 -0600

    refactored broadcast functionality

--- FCMNotification.py
+++ FCMNotification.py
@@ -31,12 +31,12 @@ def send_fcm_notification(event: dict, team: Team):
     print(F"Sent {len(tokens)} FCM messages")
     if responses.failure_count > 0 or True:
         print('Sending broadcast message since some messages did not go through')
-        message = messaging.Message(
-            data=createMessage(event),
-            topic=team.teamID,
-        )
-        message.data['description'] = 'Broadcast: ' + message.data['description']
-        response = messaging.send(message)
+        # message = messaging.Message(
+        #     data=createMessage(event),
+        #     topic=team.teamID,
+        # )
+        # message.data['description'] = 'Broadcast: ' + message.data['description']
+        # response = messaging.send(message)
         #clean up expired tokens.
         erroneous_tokens = [tokens[i] for i, response in enumerate(responses.responses) if isinstance(response.exception, UnregisteredError)]
         team.delete_tokens(erroneous_tokens)

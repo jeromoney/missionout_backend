36a4546b402379caa1e05b8177622cbc4e998350
Justin <justin.matis.com@gmail.com>
2021-03-29 15:23:03 -0600

    intitial commit of sync phone number functions

---
+++ syncphonenumber.py
@@ -0,0 +1,9 @@
+"""Temporary sync file is necessary while people have different versions of app"""
+
+
+def deletephonenumber(event: dict):
+    print(event)
+
+
+def addphonenumber(event: dict):
+    print(event)

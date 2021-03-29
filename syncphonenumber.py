dccb14a3a0a7dc5ee2a74ce56e654381c17881d8
Justin <justin.matis.com@gmail.com>
2021-03-29 17:08:39 -0600

    got sync phone numbers to work maybe

--- syncphonenumber.py
+++ syncphonenumber.py
@@ -1,9 +1,22 @@
 """Temporary sync file is necessary while people have different versions of app"""
+from google.cloud import firestore
 
 
 def deletephonenumber(event: dict):
-    print(event)
+    db = firestore.Client()
+    fields = event["oldValue"]["fields"]
+    uid = fields["uid"]["stringValue"]
+    del fields["uid"]
+    new_fields = {key: list(fields[key].values())[0] for key in fields}
+    doc_ref = db.collection("users").document(document_id=uid)
+    doc_ref.update({u"phoneNumbers": firestore.ArrayRemove([new_fields])})
 
 
 def addphonenumber(event: dict):
-    print(event)
+    db = firestore.Client()
+    fields = event["value"]["fields"]
+    uid = fields["uid"]["stringValue"]
+    del fields["uid"]
+    new_fields = {key: list(fields[key].values())[0] for key in fields}
+    doc_ref = db.collection("users").document(document_id=uid)
+    doc_ref.update({u"phoneNumbers": firestore.ArrayUnion([new_fields])})
